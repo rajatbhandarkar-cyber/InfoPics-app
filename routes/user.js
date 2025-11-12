5ba568c4b49bb8fcacdaa74dd265a9b8f77bf532
Rajat Bhandarkar <bhandarkarrajat5@mail.com>
2025-11-12 20:38:05 +0530

    latest commit

--- routes/user.js
+++ routes/user.js
@@ -49,8 +49,8 @@ router.post("/login", express.json(), (req, res, next) => {
       return res.status(401).json({
         success: false,
         error: {
-          username: "Wrong username",
-          password: "Wrong password",
+          username: "Something went wrong, please try again.",
+          password: "Something went wrong, please try again.",
         },
       });
     }
